e807927893e8a4a098f2c1d9fb8503b947bda23f
ge-arty <115204381+ge-arty@users.noreply.github.com>
2022-10-28 19:18:45 +0400

    color-palette-added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let palette = document.querySelector(".palette-btn")
+let body = document.querySelector("body")
+palette.addEventListener("click",function(){
+   let randomColor = Math.floor(Math.random()*16777215).toString(16);
+   console.log(randomColor)
+   body.style.backgroundColor = "#" + randomColor
+})
