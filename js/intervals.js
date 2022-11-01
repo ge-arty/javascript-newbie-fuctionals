24c59d6d4e7313904175be44b30e1b6367d60488
ge-arty <115204381+ge-arty@users.noreply.github.com>
2022-11-01 18:53:29 +0400

    finished

--- js/intervals.js
+++ js/intervals.js
@@ -7,26 +7,42 @@ let submit = document.querySelector(".sub-btn");
 let answerTrue = document.querySelector(".answer-true");
 let answerFalse = document.querySelector(".answer-false");
 let answerText = document.querySelector(".answer-text");
+let timeOut = document.querySelector(".out-of-time");
 let taskAnswer;
 
+// -------დარჩენილი დრო
+let timeCounter = 11;
+let myInterval = setInterval(function () {
+  timeCounter--;
+  time.innerHTML = `დარჩენილი დრო: ${timeCounter} წმ`;
+  if (timeCounter == 0) {
+    task.style.display = "none";
+    inputAnswer.style.display = "none";
+    submit.style.display = "none";
+    answerText.style.display = "none";
+    timeOut.style.display = "block";
+    clearInterval(myInterval);
+  }
+}, 1000);
+
 // --------TASK
-let number1 = Math.floor(Math.random() * 10);
-let number2 = Math.floor(Math.random() * 10);
+let number1 = Math.floor(Math.random() * (10 - 1)) + 1;
+let number2 = Math.floor(Math.random() * (10 - 1)) + 1;
 let symbol = Math.round(Math.random() * (4 - 1)) + 1;
 if (symbol == 1) {
-  task.innerHTML = `რას უდრის ${number1} + ${number2}`;
+  task.innerHTML = `რას უდრის: ${number1} + ${number2}`;
   taskAnswer = number1 + number2;
 }
 if (symbol == 2) {
-  task.innerHTML = `რას უდრის ${number1} - ${number2}`;
+  task.innerHTML = `რას უდრის: ${number1} - ${number2}`;
   taskAnswer = number1 - number2;
 }
 if (symbol == 3) {
-  task.innerHTML = `რას უდრის ${number1} / ${number2}`;
+  task.innerHTML = `რას უდრის: ${number1} / ${number2}`;
   taskAnswer = number1 / number2;
 }
 if (symbol == 4) {
-  task.innerHTML = `რას უდრის ${number1} * ${number2}`;
+  task.innerHTML = `რას უდრის: ${number1} * ${number2}`;
   taskAnswer = number1 * number2;
 }
 console.log(taskAnswer);
@@ -34,21 +50,22 @@ console.log(taskAnswer);
 // -------Answer True or False
 submit.addEventListener("click", function () {
   if (taskAnswer == inputAnswer.value) {
-    // task.style.display = "none"
-    // inputAnswer.display = "none"
+    task.style.display = "none";
+    inputAnswer.style.display = "none";
+    submit.style.display = "none";
+    answerText.style.display = "none";
+    answerTrue.style.display = "block";
+    clearInterval(myInterval);
     console.log("true");
   }
   if (taskAnswer != inputAnswer.value) {
+    task.style.display = "none";
+    inputAnswer.style.display = "none";
+    submit.style.display = "none";
+    answerText.style.display = "none";
+    answerFalse.style.display = "block";
+    clearInterval(myInterval);
+    console.log("true");
     console.log("false");
   }
 });
-// ------დარჩენილი დრო
-let timeCounter = 10;
-setInterval(function () {
-  time.innerHTML = `დარჩენილი დრო: ${timeCounter--} წმ`;
-  setTimeout(function () {
-    if ((timeCounter = 0)) {
-      clearInterval();
-    }
-  }, 10000);
-}, 1000);
